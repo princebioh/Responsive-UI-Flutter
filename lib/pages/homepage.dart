c586d12bb33d366141d8c32f3308360c27b39ecc
Prince Bioh <princebioh15@gmail.com>
2023-06-02 23:37:29 +0000

    Added ListTile in Homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -9,6 +9,16 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Responsive App"),
       ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Person ${index + 1}"),
+            trailing: Icon(Icons.pin_drop_outlined),
+          );
+        },
+        itemCount: 50,
+      ),
     );
   }
 }
